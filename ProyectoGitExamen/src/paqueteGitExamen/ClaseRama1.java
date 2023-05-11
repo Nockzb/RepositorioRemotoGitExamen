1f11a0146dd4ff8954e06bd3a4c0453a9ed60a34
Nockzb <8761804@iesplayamar.es>
2023-05-11 20:21:38 +0200

    commit rama1

---
+++ ProyectoGitExamen/src/paqueteGitExamen/ClaseRama1.java
@@ -0,0 +1,11 @@
+package paqueteGitExamen;
+
+public class ClaseRama1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Prueba 1 rama.");
+		System.out.println("Prueba 2 rama.");
+	}
+
+}
